5c43911d8c26266d24f8cbc834ef9c91059c53a9
RunaNam <nmj5012@naver.com>
2021-07-13 02:27:13 +0900

    header, footer style change

--- src/Component/Footer.js
+++ src/Component/Footer.js
@@ -8,13 +8,13 @@ import { faFacebookF, faInstagram, faTwitter } from "@fortawesome/free-brands-sv
 const Footer = styled.footer`
     height: 4rem;
     width: 100%;
-    background-color: black;
+    background-color: #000070;
 `;
 
 
 const Info = styled.div`
     color: white;
-    font-size: 1.5rem;
+    font-size: 1.3rem;
     line-height: 4rem;
     text-align: center;
 `;
@@ -23,9 +23,9 @@ export default ()=>{
     return (
         <Footer>
             <Info>
-            <FontAwesomeIcon icon={faInstagram} style={{marginRight:"20px"}}/>
-            <FontAwesomeIcon icon={faTwitter} style={{marginRight:"20px"}}/>
-            <FontAwesomeIcon icon={faFacebookF} style={{marginRight:"20px"}}/>
+            <FontAwesomeIcon icon={faInstagram} style={{marginRight:"30px"}}/>
+            <FontAwesomeIcon icon={faTwitter} style={{marginRight:"30px"}}/>
+            <FontAwesomeIcon icon={faFacebookF} />
             </Info>
         </Footer>
 
