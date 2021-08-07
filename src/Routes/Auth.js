ed2b859e4295aadfd8ed0bf59295c5d9a9dd76c2
RunaNam <nmj5012@naver.com>
2021-08-08 03:30:41 +0900

    [feat] 로그인페이지

--- src/Routes/Auth.js
+++ src/Routes/Auth.js
@@ -4,15 +4,89 @@ import { GoogleLogin } from 'react-google-login';
 import KaKaoLogin from 'react-kakao-login';
 import axios from 'axios';
 
+import Auth from "../Styles/Images/Auth.svg";
+import kakao from "../Styles/Images/kakao.svg";
+
+
 const Container = styled.div`
-    width:100%;
-    height:100%;
-    padding:35px;
-    box-sizing: border-box;
-    display: flex;
-    align-items:center;
+  display: flex;
+  flex-direction: column;
+  padding: 3% 10%;
 `;
 
+const Top = styled.div`
+  text-align: center;
+  font-size: 2rem;
+  color: #000070;
+  margin-bottom: 1rem;
+`;
+
+const Content = styled.div`
+  display: flex;
+`;  
+
+const Img = styled.img`
+
+`;
+
+const Login = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-self: center;
+  width: 20%;
+  padding-left: 10%;
+`;
+
+const Txt = styled.div`
+  color: #000070;
+    width: fit-content;
+    margin: 0 auto;
+    background-color: white;
+    padding-bottom: 2rem;
+    font-size: 1.2rem;
+`;
+const Line = styled.div`
+    background-color: #000070;
+    height: 1px;
+    width: 100%;
+    position: relative;
+    top: -2.5rem;
+    z-index: -1;
+`;
+
+const Klogo = styled.img`
+  width: 1rem;
+  margin-right: 1.5rem;
+  margin-left: 10px;
+`;
+
+const KaKaoBtn = styled(KaKaoLogin)`
+  width: 100% !important;
+  height: 40px  !important;
+  line-height: 40px  !important;
+  text-align: left !important;
+  font-size: 14px !important;
+  font-family: 'Noto Sans CJK KR' !important;
+  border-radius: 3rem !important;
+  box-shadow: rgb(0 0 0 / 24%) 0px 2px 2px 0px, rgb(0 0 0 / 24%) 0px 0px 1px 0px;
+  margin-top: 3rem;
+  &:hover {
+    opacity: 0.8;
+  }
+`;
+
+const GoogleBtn = styled(GoogleLogin)`
+  width: 100% !important;
+  height: 40px  !important;
+  line-height: 40px  !important;
+  font-size: 14px !important;
+  font-family: 'Noto Sans CJK KR';
+  border-radius: 3rem !important;
+  overflow: hidden;
+  &:hover {
+    opacity: 0.8 !important;
+  }
+`;
 
 function loginApi() {
   const url = "http://localhost:8080/api/log-in";
@@ -54,29 +128,32 @@ export default ({}) => {
 
 
   useEffect(()=>{
-    console.log(userObj);
-    localStorage.setItem("login", userObj.token);
-    console.log(localStorage.getItem('login'));
   })
-  loginApi();
 
 
     return (
         <Container>
-            <GoogleLogin 
-                clientId={"187020779465-9ie61b7h5kkniltgsjgs7nsq75g56f20.apps.googleusercontent.com"}
-                buttonText="구글로 로그인하기"
-                onSuccess={result=>googleLoginFunc(result)}
-                onFailure={result => console.log(result)}
-                cookiePolicy={'single_host_origin'}
-        />
-          <KaKaoLogin
-                token={'cb21585076043446225f15568416bac0'}
-                buttonText='카카오 계정으로 로그인'
-                onSuccess={response=>kakaoLoginFunc(response)}
-                getProfile={true}
-          />
+          <Top>환영합니다</Top>
 
+          <Content>
+            <Img src = {Auth}/>
+            <Login>
+              <Txt>간편로그인</Txt>
+              <Line/>
+              <GoogleBtn 
+                    clientId={"187020779465-9ie61b7h5kkniltgsjgs7nsq75g56f20.apps.googleusercontent.com"}
+                    onSuccess={result=>googleLoginFunc(result)}
+                    onFailure={result => console.log(result)}
+                    cookiePolicy={'single_host_origin'}
+                    buttonText = "Continue with Google"
+                />
+                <KaKaoBtn
+                      token={'cb21585076043446225f15568416bac0'}
+                      onSuccess={response=>kakaoLoginFunc(response)}
+                      getProfile={true}
+                ><Klogo src = {kakao}/> Continue with Kakao</KaKaoBtn>
+            </Login>
+            </Content>
         </Container>
     )
 }
