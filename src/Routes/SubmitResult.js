191a78ffd323c09cad6435ef64ce65dbd9c6a630
RunaNam <nmj5012@naver.com>
2021-09-01 00:58:32 +0900

    [style] 반응형 마무리 (submit)

--- src/Routes/SubmitResult.js
+++ src/Routes/SubmitResult.js
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
 import styled from "styled-components";
 import Button from '../Component/Button';
+import MobileButton from '../Component/MobileButton';
 import SubmitResult from "../Styles/Images/SubmitResult.svg";
+import SubmitResultMobile from "../Styles/Images/SubmitResultMobile.svg";
 
 
 const Container = styled.div`
@@ -15,6 +17,17 @@ const Img = styled.img`
     width: 70%;
     margin: -20% auto;
     @media ${(props)=>props.theme.tablet}{
+        width: 90%;
+        margin: -1vh auto 5vh;
+    }
+    @media ${(props)=>props.theme.mobile}{
+        display: none;
+    }
+`;
+const MobImg = styled.img`
+    display: none;
+    @media ${(props)=>props.theme.mobile}{
+        display: block;
         width: 90%;
         margin: -1vh auto 5vh;
     }
@@ -37,9 +50,12 @@ export default ({match}) => {
     return (
     <Container>
         <Img src = {SubmitResult}/>
+        <MobImg src = {SubmitResultMobile}/>
         <Btns>
             <Button backgroundColor="#7953D2" content="수정하기" marginRight="2.5rem"/>
             <Button backgroundColor="#000070" content="결과보기" onClick={()=>{window.location.replace("/#/result/"+match.params.id)}}/>
+            <MobileButton backgroundColor="#7953D2" width = "100%" content="수정하기" marginBottom="1vh"/>
+            <MobileButton backgroundColor="#000070" width = "100%" content="결과보기" onClick={()=>{window.location.replace("/#/result/"+match.params.id)}}/>
         </Btns>
     </Container>
     );
